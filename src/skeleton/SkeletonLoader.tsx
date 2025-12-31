f74e497dd1d81023a55a823bb832eba63bd300f0
Ajay k <ajaykrish212121@gmail.com>
2025-12-31 04:05:00 +0400

    Update Skeleton color

--- src/skeleton/SkeletonLoader.tsx
+++ src/skeleton/SkeletonLoader.tsx
@@ -23,28 +23,28 @@ export default function NewsSkeleton() {
           variant="rounded"
           width="100%"
           height={320}
-          sx={{ bgcolor: "#c9b4ffff" }}
+          sx={{ bgcolor: "#bfeeffff" }}
         />
 
         {/* Overlay content */}
         <Stack spacing={1} mt={2}>
-          <Skeleton variant="text" width="30%" height={24}  sx={{ bgcolor: "#c9b4ffff" }}/>
-          <Skeleton variant="text" width="90%" height={36}  sx={{ bgcolor: "#c9b4ffff" }}/>
-          <Skeleton variant="text" width="85%" height={36}  sx={{ bgcolor: "#c9b4ffff" }}/>
-          <Skeleton variant="text" width="70%" height={20}  sx={{ bgcolor: "#c9b4ffff" }}/>
+          <Skeleton variant="text" width="30%" height={24}  sx={{ bgcolor: "#bfeeffff" }}/>
+          <Skeleton variant="text" width="90%" height={36}  sx={{ bgcolor: "#bfeeffff" }}/>
+          <Skeleton variant="text" width="85%" height={36}  sx={{ bgcolor: "#bfeeffff" }}/>
+          <Skeleton variant="text" width="70%" height={20}  sx={{ bgcolor: "#bfeeffff" }}/>
 
           {/* Author row */}
           <Stack direction="row" spacing={1} alignItems="center" mt={1}>
-            <Skeleton variant="circular" width={32} height={32}  sx={{ bgcolor: "#c9b4ffff" }}/>
-            <Skeleton variant="text" width={120} height={20}  sx={{ bgcolor: "#c9b4ffff" }}/>
-            <Skeleton variant="text" width={80} height={20}  sx={{ bgcolor: "#c9b4ffff" }}/>
+            <Skeleton variant="circular" width={32} height={32}  sx={{ bgcolor: "#bfeeffff" }}/>
+            <Skeleton variant="text" width={120} height={20}  sx={{ bgcolor: "#bfeeffff" }}/>
+            <Skeleton variant="text" width={80} height={20}  sx={{ bgcolor: "#bfeeffff" }}/>
           </Stack>
         </Stack>
       </Box>
 
       {/* RIGHT: TRENDING NEWS */}
       <Box>
-        <Skeleton variant="text" width="60%" height={32} sx={{ bgcolor: "#c9b4ffff" }} />
+        <Skeleton variant="text" width="60%" height={32} sx={{ bgcolor: "#bfeeffff" }} />
 
         <Stack spacing={2} mt={2}>
           {[1, 2, 3, 4].map((i) => (
@@ -57,12 +57,12 @@ export default function NewsSkeleton() {
               <Skeleton
                 variant="rounded"
                 width={80}
-                height={60}  sx={{ bgcolor: "#c9b4ffff" }}
+                height={60}  sx={{ bgcolor: "#bfeeffff" }}
               />
               <Stack spacing={1} flex={1}>
-                <Skeleton variant="text" width="90%" height={18} sx={{ bgcolor: "#c9b4ffff" }}/>
-                <Skeleton variant="text" width="70%" height={16} sx={{ bgcolor: "#c9b4ffff" }}/>
-                <Skeleton variant="text" width="40%" height={14} sx={{ bgcolor: "#c9b4ffff" }}/>
+                <Skeleton variant="text" width="90%" height={18} sx={{ bgcolor: "#bfeeffff" }}/>
+                <Skeleton variant="text" width="70%" height={16} sx={{ bgcolor: "#bfeeffff" }}/>
+                <Skeleton variant="text" width="40%" height={14} sx={{ bgcolor: "#bfeeffff" }}/>
               </Stack>
             </Stack>
           ))}
